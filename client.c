1fb529b804cc310cd6d9afb33f23ece8c2e2a46f
PatrikDreve <patrik.drevestedt@me.com>
2015-01-26 19:13:27 +0100

    Lagt till det första raderna kod.

--- client.c
+++ client.c
@@ -6,3 +6,20 @@
 //  Copyright (c) 2015 WorkhorsePS. All rights reserved.
 //
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(char 'msg) {
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+}
