8bc0643260b1eb3efc4effb60cdf0b57b00fb21d
it4e <limpands@gmail.com>
2015-01-27 20:53:35 +0100

    Rättade till några slarvfel från föregående commit

--- server.c
+++ server.c
@@ -7,6 +7,8 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "server.h"
 #include <sys/types.h>
 #include <netinet/in.h>
--- server.h
+++ server.h
@@ -9,6 +9,10 @@
 #ifndef __HTTPServer__server__
 #define __HTTPServer__server__
 
+// Maximalt antal klienter som kan ansluta
+#define BACKLOG 2000
+
 void error(char *msg);
+void server_config(int * server, int port);
 
 #endif /* defined(__HTTPServer__server__) */
