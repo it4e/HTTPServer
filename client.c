9cafc37f66da157a4d1449a2075f3f2e3503ff51
PatrikDreve <patrik.drevestedt@me.com>
2015-01-27 19:42:54 +0100

    Ännu några rader kod.

--- client.c
+++ client.c
@@ -12,7 +12,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-void error(char 'msg) {
+void error(char *msg) {
     perror(msg);
     exit(0);
 }
@@ -22,4 +22,16 @@ int main(int argc, char *argv[]) {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    
+    char buffer[256];
+    
+    if (argc < 3){
+        fprintf(stderr, "usage hostname %s port\n", argv[0]);
+        exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0){
+        error("ERROR opening socket");
+    }
 }
